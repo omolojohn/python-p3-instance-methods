d94409543c951b24e7982f9318daf82da50f2bc6
omolo-j <omoloj135@gmail.com>
2024-05-22 21:27:11 +0300

    instance methods

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,13 @@
 
 class Dog:
     # Class body goes here
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+if __name__ == "__main__":
+    my_dog = Dog()
+    my_dog.sit()
+    my_dog.bark()
